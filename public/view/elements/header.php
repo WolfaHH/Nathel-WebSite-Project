c6ff452e0045f6b1e1b7122ff9b3adee97441b8a
Elliot <elliot.decle@gmail.com>
2020-11-29 20:15:29 +0100

    Update : Header

--- public/view/elements/header.php
+++ public/view/elements/header.php
@@ -1,15 +1,76 @@
-<html>
+<!doctype html>
+<html lang=fr>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="#" rel="stylesheet" type="text/css">
     <title>Document</title>
 </head>
 <body>
-<header>
-    <nav>
-        <span>owo</span>
+<header id="header">
 
+    <img class="header-logo d-none d-sm-block" src="#" alt="notre super logo">
+
+    <nav class="navbar">
+        <ul>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link active">
+                    <!--
+                    <span class="material-icons">
+                        home
+                    </span>
+                    -->
+                    Home
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link active">
+                    icon
+                    Pools
+                </a>
+            </li>
+
+        </ul>
     </nav>
+
+    <div class="d-none d-sm-block ml-auto">
+        twitter
+        discord
+
+        <form class="form-header ">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn" type="submit">
+                icon search
+            </button>
+        </form>
+
+    </div>
+
+    <div class="nav-item">
+        <a href="#" class="nav-link active">
+            icon
+            Home
+        </a>
+    </div>
+
+    <div class="d-sm-none search-mobile">
+
+        <img class="header-logo" src="#" alt="notre super logo">
+
+        <form class="form-header">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn" type="submit">
+                icon search
+            </button>
+        </form>
+
+    </div>
+
 </header>
+
+
+
