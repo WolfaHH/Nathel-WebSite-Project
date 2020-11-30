8c6f2f282c78c245d6ff04dfaecc5ab35841ba86
Elliot <elliot.decle@gmail.com>
2020-11-30 21:43:43 +0100

    Lesson : foreach 2 method

--- public/view/template/home.php
+++ public/view/template/home.php
@@ -4,13 +4,25 @@ $popular_pools[0] = ['name' => 'KimiPool Challenge',
     'from' => 'Dan project : Aim collection',
     'submitter' => 'Nathanael',
     'nb_map' => 45,
-    'categories' => [[tournament, 1], [rank_range, 3], [Aim, 0]]
+    'categories' => [
+        ['id' => 1,
+            'name' => 'tournament',
+            'type' => 'category'],
+        ['id' => 2,
+            'name' => 'Aim',
+            'type' => 'custom_tag'],
+        ['id' => 3,
+            'name' => '100k-250k',
+            'type' => 'rank_range']]
 ];
 $popular_pools[1] = ['name' => 'KimiPool Challenge',
     'from' => 'Dan project : Aim collection',
     'submitter' => 'Nathanael',
     'nb_map' => 45,
-    'categories' => [['tournament', 1], ['rank_range', 3], ['Aim', 0]]
+    'categories' => [
+        'category' => 'tournament',
+        'rank_range' => '100k-250k',
+        'custom_tag' => 'Aim']
 ];
 #...
 
@@ -27,14 +39,26 @@ $popular_pools[1] = ['name' => 'KimiPool Challenge',
 
     <div class="container">
         <h2>Most popular mappools</h2>
-        <?php for ($i = 0; $i <= 5; $i++) { ?>
+        <?php for ($i = 0; $i <= 5; $i++) : ?>
         <div class="mostpopular <?php $i ?>">
         <span><?php echo $popular_pools[$i] ?></span>
         <span>from <?php echo $popular_pools[$i]['from'] ?></span>
         <span>submitted by <?php echo $popular_pools[$i]['submitter'] ?></span>
         <span> <?php echo $popular_pools[$i]['nb_map'] ?></span>
         <span>
-            <?php foreach ($popular_pools[$i]['categories'] as $index => $category ){}?>
+
+            <?php foreach ($popular_pools[$i]['categories'] as $category ):?>
+
+            <span class="color-<?= $category['type'] ?>"><?= $category['name'] ?></span>
+
+            <?php endforeach; ?>
+
+            
+            <?php foreach ($popular_pools[$i]['categories'] as $index => $category ):?>
+
+                <span class="color-<?= $index ?>"><?= $category ?></span>
+
+            <?php endforeach; ?>
         </span>
         <form class="form-button">
             <button class="btn" type="submit">
@@ -49,7 +73,7 @@ $popular_pools[1] = ['name' => 'KimiPool Challenge',
         </form>
     </div>
             
-        <?php }?>
+        <?php endfor; ?>
 
 
     </div>
