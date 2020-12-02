f01654ce493f6706b29724d9fc627e6d22c3fca1
Elliot <elliot.decle@gmail.com>
2020-12-02 16:23:40 +0100

    Edit : assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,8 @@ let mix = require('laravel-mix');
  */
 
 mix
-    .js('src/js/app.js', 'assets/js')
-    .sass('src/sass/app.scss', 'assets/css');
+    .js('src/js/app.js', 'public/assets/js')
+    .sass('src/sass/app.scss', 'public/assets/css');
 
 // Full API
 // mix.js(src, output);
