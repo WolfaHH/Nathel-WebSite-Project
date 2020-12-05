3ba257614427c8086017079f829dc9017c886a6a
Nath <godard.nathanael@gmail.com>
2020-12-05 17:40:38 +0100

    Update : Templating Page home

--- public/view/template/home.php
+++ public/view/template/home.php
@@ -1,31 +1,6 @@
-<?php
-
-$popular_pools[0] = ['name' => 'KimiPool Challenge',
-    'from' => 'Dan project : Aim collection',
-    'submitter' => 'Nathanael',
-    'nb_map' => 45,
-    'categories' => [
-        ['id' => 1,
-            'name' => 'tournament',
-            'type' => 'category'],
-        ['id' => 2,
-            'name' => 'Aim',
-            'type' => 'custom_tag'],
-        ['id' => 3,
-            'name' => '100k-250k',
-            'type' => 'rank_range']]
-];
-$popular_pools[1] = ['name' => 'KimiPool Challenge',
-    'from' => 'Dan project : Aim collection',
-    'submitter' => 'Nathanael',
-    'nb_map' => 45,
-    'categories' => [
-        'category' => 'tournament',
-        'rank_range' => '100k-250k',
-        'custom_tag' => 'Aim']
-];
-#...
-
+<!-- IMPORT -->
+<?php require_once('../controller/backend/home.php');
+$display_pools = $display_pools;
 ?>
 
 <main>
@@ -36,45 +11,72 @@ $popular_pools[1] = ['name' => 'KimiPool Challenge',
             Continue with osu!
         </button>
     </form>
+    <?php for ($d = 0; $d <=1; $d++) : ?>
 
     <div class="container">
-        <h2>Most popular mappools</h2>
-        <?php for ($i = 0; $i <= 5; $i++) : ?>
+        <h2> <?php echo  $d === 0 ? 'Most popular mappools' : 'Most recent mappools' ?></h2>
+        <?php for ($i = 0; $i <= 4; $i++) : ?>
         <div class="mostpopular <?php $i ?>">
-        <span><?php echo $popular_pools[$i] ?></span>
-        <span>from <?php echo $popular_pools[$i]['from'] ?></span>
-        <span>submitted by <?php echo $popular_pools[$i]['submitter'] ?></span>
-        <span> <?php echo $popular_pools[$i]['nb_map'] ?></span>
+        <span><?php echo $display_pools[$i]['name'] ?></span>
+        <span>from <?php echo $display_pools[$i]['from'] ?></span>
+        <span>submitted by <?php echo $display_pools[$i]['submitter'] ?></span>
+        <span> <?php echo $display_pools[$i]['nb_map'] ?> maps</span>
         <span>
-
-            <?php foreach ($popular_pools[$i]['categories'] as $category ):?>
-
-            <span class="color-<?= $category['type'] ?>"><?= $category['name'] ?></span>
-
-            <?php endforeach; ?>
-
-            
-            <?php foreach ($popular_pools[$i]['categories'] as $index => $category ):?>
-
-                <span class="color-<?= $index ?>"><?= $category ?></span>
-
+            <?php foreach ($display_pools[$i]['categories'] as $category => $value ):?>
+            <span class="color-<?= $category ?>"><?= $value ?></span>
             <?php endforeach; ?>
         </span>
         <form class="form-button">
             <button class="btn" type="submit">
-                fleche svg
+                fleche && svg
                 Follow
             </button>
         </form>
         <form class="fleche">
             <button class="btn" type="submit">
-                icone svg fleche
+                icone svg fleche deroulante
             </button>
         </form>
-    </div>
-            
+        </div>
+        <div>
+        <?php foreach ($display_pools[$i]['maps'] as $map ):?>
+        <div>
+            <span><?php echo $map['name']?></span>
+            <span>mappée par <?php echo $map['mapper']?></span>
+            + image bg et logo mod
+        </div>
+        <?php endforeach; ?>
+
+        </div>
+        <form class="form_button">
+            <button class="btn" type="submit">
+                Show Mappool
+            </button>
+        </form>
+
         <?php endfor; ?>
+    </div>
+    <?php endfor; ?>
 
+    <div class="container">
+        <h2>Who are we ?</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultrices urna sed elit tempus pulvinar. Phasellus elementum eros tincidunt lacinia placerat. Ut non lectus a tellus faucibus elementum. Ut ultricies lacinia massa, nec iaculis risus tincidunt vitae. In tincidunt aliquam felis, at efficitur eros condimentum vitae. Maecenas vehicula, elit sit amet elementum scelerisque, urna orci venenatis neque, nec malesuada justo turpis sit amet ante.</p>
+        <div>
+            image profil, nom et description + bouton know more X2
+        </div>
+    </div>
 
+    <div class="container">
+        <h2>Improve !</h2>
+        <div> img stylé, slogan 1</div>
+        <div> img stylé, slogan 2</div>
+        <div> img stylé, slogan 3</div>
+        <h2>& Create !</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultrices urna sed elit tempus pulvinar. Phasellus elementum eros tincidunt lacinia placerat. Ut non lectus a tellus faucibus elementum. Ut ultricies lacinia massa, nec iaculis risus tincidunt vitae. In tincidunt aliquam felis, at efficitur eros condimentum vitae. Maecenas vehicula, elit sit amet elementum scelerisque, urna orci venenatis neque, nec malesuada justo turpis sit amet ante.</p>
+        <form class="form-button">
+            <button class="btn" type="submit">
+                Create my first mappool now !
+            </button>
+        </form>
     </div>
 </main>
